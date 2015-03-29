8a7bf593a8a75e0378ef815c58f04c75e5e90c31
Rory O’Kane <rory@roryokane.com>
2015-03-28 23:06:56 -0400

    Implement skipping a tick

--- scripts/main.js
+++ scripts/main.js
@@ -48,11 +48,13 @@ jQuery(function() {
 		normalTickDelay: 1000,
 		probabilityOfAccuracyEachTick: 0.75,
 		probabilityOfTogglingTickTock: 0.9,
+		probabilityOfSkippingATickWhenPossible: 0.75,
+		probabilityOfSkewingPositive: 0.3,
 		possibleSkewPerInaccurateTick: {
 			minimum: 100,
 			maximum: 250,
 		},
-		maximumAllowedTotalSkew: 999,
+		maximumAllowedTotalSkew: 1499,
 	};
 	sanityCheckTickTimingConfig(tickTimingConfig);
 	
@@ -60,11 +62,18 @@ jQuery(function() {
 	
 	
 	function sanityCheckTickTimingConfig(ttc) {
-		if (ttc.normalTickDelay <= 0 ||
-		    ttc.probabilityOfAccuracyEachTick < 0 || ttc.probabilityOfAccuracyEachTick > 1 ||
-		    ttc.possibleSkewPerInaccurateTick.minimum < 0 ||
-		    ttc.possibleSkewPerInaccurateTick.maximum < ttc.possibleSkewPerInaccurateTick.minimum ||
-		    ttc.possibleSkewPerInaccurateTick.maximum > ttc.maximumAllowedTotalSkew) {
+		var passedSanityCheck = (
+			ttc.normalTickDelay > 0 &&
+			ttc.probabilityOfAccuracyEachTick >= 0 && ttc.probabilityOfAccuracyEachTick <= 1 &&
+			ttc.probabilityOfTogglingTickTock >= 0 && ttc.probabilityOfTogglingTickTock <= 1 &&
+			ttc.probabilityOfSkippingATickWhenPossible >= 0 && ttc.probabilityOfSkippingATickWhenPossible <= 1 &&
+			ttc.probabilityOfSkewingPositive >= 0 && ttc.probabilityOfSkewingPositive <= 0.5 &&
+			ttc.possibleSkewPerInaccurateTick.minimum >= 0 &&
+			ttc.possibleSkewPerInaccurateTick.maximum >= ttc.possibleSkewPerInaccurateTick.minimum &&
+			ttc.possibleSkewPerInaccurateTick.maximum <= ttc.maximumAllowedTotalSkew &&
+			ttc.maximumAllowedTotalSkew >= ((2*ttc.normalTickDelay + ttc.possibleSkewPerInaccurateTick.maximum) / 2)
+		);
+		if (!passedSanityCheck) {
 			throw "tickTimingConfig failed sanity check";
 		}
 	}
@@ -93,6 +102,18 @@ jQuery(function() {
 	}
 	
 	function generateSkewForInaccurateNextTick() {
+		var potentialSkippingSkew = generateSkewForSkippingNextTickIgnoringTotalSkew();
+		
+		var potentialTotalSkew = millisecondsOfTotalSkew + potentialSkippingSkew;
+		var isSkippingATickPossible = totalSkewIsWithinAllowedRange(potentialTotalSkew);
+		if (isSkippingATickPossible && trueWithProbability(ttc.probabilityOfSkippingATickWhenPossible)) {
+			return potentialSkippingSkew;
+		} else {
+			return generateSkewForInaccurateNonSkippingNextTick();
+		}
+	}
+		
+	function generateSkewForInaccurateNonSkippingNextTick() {
 		var potentialSkew = generateSkewForInaccurateNextTickIgnoringTotalSkew();
 		
 		var potentialTotalSkew = millisecondsOfTotalSkew + potentialSkew;
@@ -105,11 +126,18 @@ jQuery(function() {
 	}
 	
 	function generateSkewForInaccurateNextTickIgnoringTotalSkew() {
-		var minimum = ttc.possibleSkewPerInaccurateTick.minimum;
-		var maximum = ttc.possibleSkewPerInaccurateTick.maximum;
-		var absoluteSkew = _.random(minimum, maximum);
-		var skewShouldBePositive = trueWithProbability(0.5);
+		var minimumSkew = ttc.possibleSkewPerInaccurateTick.minimum;
+		var maximumSkew = ttc.possibleSkewPerInaccurateTick.maximum;
+		var absoluteSkew = _.random(minimumSkew, maximumSkew);
+		var skewShouldBePositive = trueWithProbability(ttc.probabilityOfSkewingPositive);
 		return absoluteSkew * (skewShouldBePositive ? 1 : -1);
+	}
+	
+	function generateSkewForSkippingNextTickIgnoringTotalSkew() {
+		var doubleTickDelay = ttc.normalTickDelay * 2;
+		var minimumSkew = doubleTickDelay - ttc.possibleSkewPerInaccurateTick.maximum;
+		var maximumSkew = doubleTickDelay + ttc.possibleSkewPerInaccurateTick.maximum;
+		return _.random(minimumSkew, maximumSkew);
 	}
 	
 	function totalSkewIsWithinAllowedRange(totalSkew) {
